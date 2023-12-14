10b6f32b07242b9951650edcad88ff0cb4ab1586
Joseph Clark <weirdlookingjay@live.com>
2023-12-14 16:10:19 -0500

    Created a collapsible sidebar component with tooltip

--- app/(browse)/_components/navbar/actions.tsx
+++ app/(browse)/_components/navbar/actions.tsx
@@ -9,7 +9,7 @@ const Actions = async () => {
   return (
     <div className="flex items-center justify-end gap-x-2 ml-4 lg:ml-0">
       {!user && (
-        <SignInButton>
+        <SignInButton mode="modal">
           <Button size="sm" variant={"primary"}>
             Login
           </Button>
--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -12,11 +12,11 @@ const font = Poppins({
 const Logo = () => {
   return (
     <Link href="/">
-      <div className="hidden lg:flex items-center gap-x-4 hover:opacity-75 transition">
-        <div className="bg-white rounded-full p-1">
+      <div className="flex items-center gap-x-4 hover:opacity-75 transition">
+        <div className="bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
           <Image src="/spooky.svg" alt="Gaminghub" height="32" width="32" />
         </div>
-        <div className={cn(font.className)}>
+        <div className={cn("hidden lg:block ", font.className)}>
           <p className="text-lg font-semibold">Gaminghub</p>
           <p className="text-xs text-muted-foreground">Let&apos;s play</p>
         </div>
